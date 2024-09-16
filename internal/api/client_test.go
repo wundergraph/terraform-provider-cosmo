53e677fb2115f1a829f62dc6dbbd2427cc265040
Andreas Zeissner <andi@wundergraph.com>
2024-09-16 09:57:12 +0200

    chore: remove apikey from client test

--- internal/api/client_test.go
+++ internal/api/client_test.go
@@ -11,7 +11,7 @@ func TestNewClient(t *testing.T) {
 	t.Setenv("COSMO_API_URL", "https://env-url.com")
 
 	client, err := api.NewClient("", "")
-	if err != nil || client.CosmoApiKey != "env_api_key" || client.Client == nil {
+	if err != nil || client.Client == nil {
 		t.Errorf("Expected client with env variables, got error: %v", err)
 	}
 
@@ -19,7 +19,7 @@ func TestNewClient(t *testing.T) {
 	t.Setenv("COSMO_API_URL", "")
 
 	client, err = api.NewClient("passed_api_key", "https://passed-url.com")
-	if err != nil || client.CosmoApiKey != "passed_api_key" || client.Client == nil {
+	if err != nil || client.Client == nil {
 		t.Errorf("Expected client with passed variables, got error: %v", err)
 	}
 }
