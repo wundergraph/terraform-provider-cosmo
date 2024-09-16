2458905c94c8136c8a69ba5a62f659c29c137b40
Andreas Zeissner <andi@wundergraph.com>
2024-09-16 09:07:43 +0200

    chore: fix linting
    os.Setenv() can be replaced by `t.Setenv()` in TestNewClient (tenv)

--- internal/api/client_test.go
+++ internal/api/client_test.go
@@ -1,23 +1,22 @@
 package api_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/wundergraph/cosmo/terraform-provider-cosmo/internal/api"
 )
 
 func TestNewClient(t *testing.T) {
-	os.Setenv("COSMO_API_KEY", "env_api_key")
-	os.Setenv("COSMO_API_URL", "https://env-url.com")
+	t.Setenv("COSMO_API_KEY", "env_api_key")
+	t.Setenv("COSMO_API_URL", "https://env-url.com")
 
 	client, err := api.NewClient("", "")
 	if err != nil || client.CosmoApiKey != "env_api_key" || client.Client == nil {
 		t.Errorf("Expected client with env variables, got error: %v", err)
 	}
 
-	os.Unsetenv("COSMO_API_KEY")
-	os.Unsetenv("COSMO_API_URL")
+	t.Setenv("COSMO_API_KEY", "")
+	t.Setenv("COSMO_API_URL", "")
 
 	client, err = api.NewClient("passed_api_key", "https://passed-url.com")
 	if err != nil || client.CosmoApiKey != "passed_api_key" || client.Client == nil {
