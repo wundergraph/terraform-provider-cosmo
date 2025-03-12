a7ef159501fd06cc3d69bc54d495fa893b54e786
Ludwig Bedacht <ludwig.bedacht@gmail.com>
2025-03-12 10:49:41 +0100

    chore: add test

--- internal/service/feature-subgraph/resource_cosmo_feature_subgraph_test.go
+++ internal/service/feature-subgraph/resource_cosmo_feature_subgraph_test.go
@@ -2,6 +2,7 @@ package feature_subgraph_test
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
@@ -116,6 +117,32 @@ func TestAccFeatureSubgraphWithoutSchema(t *testing.T) {
 	})
 }
 
+func TestAccFeatureSubgraphWithoutBaseSubgraphName(t *testing.T) {
+	fsgName := acctest.RandomWithPrefix("test-feature-subgraph")
+	namespace := acctest.RandomWithPrefix("test-namespace")
+
+	fgName, sgName :=
+		acctest.RandomWithPrefix("test-federated-graph"),
+		acctest.RandomWithPrefix("test-subgraph")
+
+	routingURL := "https://example.com"
+
+	subgraphSchema := acceptance.TestAccValidSubgraphSchema
+	readme := "Initial readme content"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acceptance.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acceptance.TestAccProtoV6ProviderFactories,
+
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccFeatureSubgraphResourceConfigNoBaseSubgraph(namespace, fgName, sgName, fsgName, routingURL, subgraphSchema, readme),
+				ExpectError: regexp.MustCompile(".*A feature subgraph requires a base.*"),
+			},
+		},
+	})
+}
+
 // TODO: uncomment when URL normalization was fixed in the control plane
 //func TestAccFeatureSubgraphRoutingURL(t *testing.T) {
 //	t.Run("should contain the correct state when providing localhost without protocol", func(t *testing.T) {
@@ -197,6 +224,25 @@ resource "cosmo_feature_subgraph" "test" {
   namespace           = cosmo_namespace.test.name
   routing_url         = "%s"
   base_subgraph_name  = cosmo_subgraph.test.name
+  schema              = <<-EOT
+%sEOT
+  readme              = "%s"
+
+  depends_on = [cosmo_subgraph.test]
+}
+`, formatBaseResource(namespace, fgName, sgName), fsgName, fsgRoutingURL, fsgSchema, fsgReadme)
+}
+
+func testAccFeatureSubgraphResourceConfigNoBaseSubgraph(
+	namespace, fgName, sgName, fsgName, fsgRoutingURL, fsgSchema, fsgReadme string) string {
+	return fmt.Sprintf(`
+%s
+
+resource "cosmo_feature_subgraph" "test" {
+  name                = "%s"
+  namespace           = cosmo_namespace.test.name
+  routing_url         = "%s"
+  base_subgraph_name  = ""
   schema              = <<-EOT
 %sEOT
   readme              = "%s"
