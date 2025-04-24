f4edbc58931998605a0b756af88e766b2d63b894
Ludwig Bedacht <ludwig.bedacht@gmail.com>
2025-04-24 19:05:03 +0200

    fix: also retrieve the subgraph schema in the cosmo_subgraph data source

--- internal/service/subgraph/data_source_cosmo_subgraph_test.go
+++ internal/service/subgraph/data_source_cosmo_subgraph_test.go
@@ -17,13 +17,13 @@ func TestAccSubgraphDataSource(t *testing.T) {
 
 	subgraphName := acctest.RandomWithPrefix("test-subgraph")
 	subgraphRoutingURL := "https://example.com"
-
+	subgraphSchema := acceptance.TestAccValidSubgraphSchema
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { acceptance.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: acceptance.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSubgraphDataSourceConfig(namespace, federatedGraphName, federatedGraphRoutingURL, subgraphName, subgraphRoutingURL),
+				Config: testAccSubgraphDataSourceConfig(namespace, federatedGraphName, federatedGraphRoutingURL, subgraphName, subgraphRoutingURL, subgraphSchema),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "name", subgraphName),
 					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "namespace", namespace),
@@ -34,13 +34,61 @@ func TestAccSubgraphDataSource(t *testing.T) {
 					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "subscription_protocol", "ws"),
 					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "websocket_subprotocol", "auto"),
 					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "is_event_driven_graph", "false"),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "schema", subgraphSchema),
+				),
+			},
+			{
+				Config:  testAccSubgraphDataSourceConfig(namespace, federatedGraphName, federatedGraphRoutingURL, subgraphName, subgraphRoutingURL, subgraphSchema),
+				Destroy: true,
+			},
+			{
+				Config: testAccSubgraphDataSourceConfigWithoutSchema(namespace, federatedGraphName, federatedGraphRoutingURL, subgraphName, subgraphRoutingURL),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "name", subgraphName),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "namespace", namespace),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "routing_url", subgraphRoutingURL),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "labels.team", "backend"),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "labels.stage", "dev"),
+					resource.TestCheckNoResourceAttr("data.cosmo_subgraph.test", "subscription_url"),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "subscription_protocol", "ws"),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "websocket_subprotocol", "auto"),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "is_event_driven_graph", "false"),
+					resource.TestCheckResourceAttr("data.cosmo_subgraph.test", "schema", ""),
 				),
 			},
 		},
 	})
 }
 
-func testAccSubgraphDataSourceConfig(namespace, federatedGraphName, federatedGraphroutingURL, subgraphName, subgraphRoutingURL string) string {
+func testAccSubgraphDataSourceConfig(namespace, federatedGraphName, federatedGraphroutingURL, subgraphName, subgraphRoutingURL, subgraphSchema string) string {
+	return fmt.Sprintf(`
+resource "cosmo_namespace" "test" {
+  name = "%s"
+}
+
+resource "cosmo_federated_graph" "test" {
+  name      	= "%s"
+  namespace 	= cosmo_namespace.test.name
+  routing_url 	= "%s"
+}
+
+resource "cosmo_subgraph" "test" {
+  name                = "%s"
+  namespace           = cosmo_namespace.test.name
+  routing_url         = "%s"
+  labels              = { "team" = "backend", "stage" = "dev" }
+  schema              = <<-EOT
+%sEOT
+}
+
+data "cosmo_subgraph" "test" {
+  name      = cosmo_subgraph.test.name
+  namespace = cosmo_subgraph.test.namespace
+}
+`, namespace, federatedGraphName, federatedGraphroutingURL, subgraphName, subgraphRoutingURL, subgraphSchema)
+}
+
+func testAccSubgraphDataSourceConfigWithoutSchema(namespace, federatedGraphName, federatedGraphroutingURL, subgraphName, subgraphRoutingURL string) string {
 	return fmt.Sprintf(`
 resource "cosmo_namespace" "test" {
   name = "%s"
